b936124438532c600609c0b1022ff2339ff817be
snehap1011 <sneha.p@experionglobal.com>
2021-11-28 23:11:55 +0530

    Added resource interface

---
+++ TrainingManagementRestAPI/Repository/IResourceRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TrainingManagementRestAPI.Models;
+
+namespace TrainingManagementRestAPI.Repository
+{
+   public  interface IResourceRepository
+    {
+        //Get all Resources
+        Task<List<TblResource>> GetResource();
+
+        //Add a new Resource
+        Task<int> AddResource(TblResource resource);
+
+        //Update Resource
+        Task UpdateResource(TblResource resource);
+
+        //Delete Resource
+        Task DeleteResource(int id);
+     
+        //Get resource by id
+        Task<TblResource> GetResourceById(int id);
+
+    }
+}
