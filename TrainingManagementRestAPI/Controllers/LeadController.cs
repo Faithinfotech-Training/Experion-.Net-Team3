ae056ab1676eb0266d1d68ef3cb131fdc8446461
Ann Mary Jose <ann.jose@experionglobal.com>
2021-11-29 21:07:47 +0530

    commit the lead

---
+++ TrainingManagementRestAPI/Controllers/LeadController.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TrainingManagementRestAPI.Models;
+using TrainingManagementRestAPI.Repository;
+
+namespace TrainingManagementRestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeadController : ControllerBase
+    {
+        ILeadRepository leadRepository;
+        //constructor dependency injection
+        public LeadController(ILeadRepository _c)
+        {
+            leadRepository = _c;
+        }
+        #region Get lead
+        [HttpGet]
+        [Route("GetLead")]
+        public async Task<IActionResult> GetLead()
+        {
+
+            try
+            {
+                var lead = await leadRepository.GetLead();
+                if (lead == null)
+                {
+                    return NotFound();
+                }
+                return Ok(lead);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+        #endregion
+
+        #region Add lead
+        [HttpPost]
+        [Route("AddLead")]
+        public async Task<IActionResult> AddLead([FromBody] TblLead model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var LeadId = await leadRepository.AddLead(model);
+                    if (LeadId > 0)
+                    {
+                        return Ok(LeadId);
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
+                catch (Exception)
+                {
+                    return BadRequest();
+                }
+            }
+            return BadRequest();
+
+        }
+        #endregion
+
+        #region Update Lead
+        [HttpPut]
+        [Route("UpdateLead")]
+        public async Task<IActionResult> UpdateLead([FromBody] TblLead model)
+        {
+            //check the validation of body
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await leadRepository.UpdateLead(model);
+                    return Ok();
+                }
+                catch (Exception)
+                {
+                    return BadRequest();
+                }
+            }
+            return BadRequest();
+        }
+        #endregion
+
+        #region Delete Course
+        [HttpDelete]
+        [Route("DeleteLead")]
+        //[Route("DeleteLead")]
+        public async Task<IActionResult> DeleteLead(int id)
+        {
+            try
+            {
+                var lead= await leadRepository.DeleteLead(id);
+                if (lead == null)
+                {
+                    return NotFound();
+                }
+                return Ok(lead);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+        #endregion
+
+        #region GetLeadId
+       
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCourseById(int id)
+
+        {
+
+            try
+
+            {
+
+                var course = await leadRepository.GetLeadById(id);
+
+                if (course == null)
+
+                {
+
+                    return NotFound();
+
+                }
+
+                return Ok(course);
+
+            }
+
+            catch (Exception)
+
+            {
+
+                return BadRequest();
+
+            }
+        }
+
+        #endregion
+        #region Add lead
+        [HttpPost]
+        [Route("AddUser")]
+        public async Task<IActionResult> AddUser([FromBody] TblUser model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var UserId = await leadRepository.AddUser(model);
+                    if (UserId > 0)
+                    {
+                        return Ok(UserId);
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
+                catch (Exception)
+                {
+                    return BadRequest();
+                }
+            }
+            return BadRequest();
+
+        }
+        #endregion
+    }
+}
+
---
+++ TrainingManagementRestAPI/Repository/LeadRepository.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TrainingManagementRestAPI.Models;
+
+namespace TrainingManagementRestAPI.Repository
+{
+    public class LeadRepository:ILeadRepository
+    {
+
+        Training_AcademyDBContext db;
+
+        public LeadRepository(Training_AcademyDBContext _db)
+        {
+            db = _db;
+        }
+
+        #region Getlead()
+        public async Task<List<TblLead>> GetLead()
+        {
+            if (db != null)
+            {
+                return await db.TblLead.ToListAsync();
+            }
+            return null;
+        }
+        #endregion
+
+        #region AddLead()
+        public async Task<int> AddLead(TblLead lead)
+        {
+            if (db != null)
+            {
+                await db.TblLead.AddAsync(lead);
+                await db.SaveChangesAsync();
+                return lead.LeadId;
+            }
+            return 0;
+        }
+        #endregion
+
+        #region DeleteLead()
+        public async Task<TblLead> DeleteLead(int id)
+        {
+            if (db != null)
+            {
+                TblLead dbLead = db.TblLead.Find(id);
+                db.TblLead.Remove(dbLead);
+                db.SaveChanges();
+                return dbLead;
+            }
+            return null;
+        }
+        #endregion
+
+        #region UpdateLead()
+        public async Task UpdateLead(TblLead lead)
+        {
+            if (db != null)
+            {
+                db.TblLead.Update(lead);
+                await db.SaveChangesAsync();
+            }
+        }
+        #endregion
+
+        #region Get Lead by id
+        public async Task<TblLead> GetLeadById(int id)
+        {
+            if (db != null)
+            {
+                TblLead lead = await db.TblLead.FindAsync(id);
+                return lead;
+
+            }
+            return null;
+        }
+        #endregion
+
+        #region AddUser()
+        public async Task<int> AddUser(TblUser user)
+        {
+            if (db != null)
+            {
+
+                await db.TblUser.AddAsync(user);
+                await db.SaveChangesAsync();
+                return user.LoginId;
+            }
+            return 0;
+        }
+
+       
+        #endregion
+
+    }
+}
